ffc8dda034752bcfe309e3b2337345d0bf2e296a
sathish <sathish12.vs@gmail.com>
2023-05-08 16:08:42 +0530

    sa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ScrollSpyService } from 'ng-spy';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'language';
-}
+  title!: string;
+  emailId!: string;
+  constructor(private spyService:ScrollSpyService,
+    public translate: TranslateService
+  ) {
+    translate.addLangs(['en', 'es', 'fr']);
+    translate.setDefaultLang('en');
+  }
+  translateLanguageTo(lang: string) {
+    this.translate.use(lang);
+ 
+  }
+    getTitleString() {
+      this.translate.get(['TITLE', 'USER_INFO.EMAIL_ADDRESS'])
+        .subscribe(translations => {
+          this.title = translations['TITLE'];
+          this.emailId = translations['USER_INFO.EMAIL_ADDRESS'];
+        });
+    }
+
+    ngAfterViewInit() {
+      this.spyService.spy({ thresholdBottom: 50 });
+    }
+  
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ScrollSpyModule } from 'ng-spy';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ScrollSpyModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
